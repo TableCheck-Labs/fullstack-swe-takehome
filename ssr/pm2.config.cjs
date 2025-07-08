76f850af56d6d7ad0b4d7100b6c4397786ca2eb1
Daniel Lizik <10506366+daniellizik@users.noreply.github.com>
2025-07-09 00:13:28 +0900

    pm2

---
+++ ssr/pm2.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "ssr",
+      script: "./dist/server/index.js",
+      instances: "max",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
